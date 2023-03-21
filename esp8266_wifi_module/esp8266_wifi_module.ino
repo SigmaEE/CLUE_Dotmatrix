439a036fe751e97df02c013783ce8841d1e39f21
Calle Lindquist <calle.lindquist@sigma.se>
2023-03-21 15:29:44 +0100

    WifiModule: Add forwarding of messages received from clients

--- esp8266_wifi_module/esp8266_wifi_module.ino
+++ esp8266_wifi_module/esp8266_wifi_module.ino
@@ -1,7 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
-#define DEBUG 1
+#define DEBUG 0
 
 #if DEBUG
   #define DEBUG_PRINT(x) Serial.print(x)
@@ -28,10 +28,11 @@ const uint8_t game_of_life_config_message_identifier = 0x11;
 
 enum response_code {
   RESPONSE_OK = 0x01,
-  RESPONSE_TARGET_BUSY = 0x02,
+  RESPONSE_INTERNAL_ERROR = 0x02,
   RESPONSE_CHECKSUM_MISMATCH = 0x03,
   RESPONSE_TIMEOUT = 0x04,
-  RESPONSE_UNEXPECTED_RESPONSE = 0x05
+  RESPONSE_TARGET_UNEXPECTED_DATA = 0x05,
+  RESPONSE_UNEXPECTED = 0x06
 };
 
 WiFiUDP udp;
@@ -39,6 +40,8 @@ WiFiServer server(local_ip_address, server_port);
 
 void setup() {
   Serial.begin(115200);
+  Serial.setTimeout(3000);
+
   DEBUG_PRINTLN("Setting up device as access point...");  
   bool result = WiFi.softAPConfig(local_ip_address, gateway, net_mask);
   if (!result) {
@@ -76,22 +79,20 @@ void send_response(WiFiClient& client, response_code response) {
 }
 
 uint16_t compute_crc16_checksum(uint8_t* data, uint16_t length) {
-    // See https://en.wikipedia.org/wiki/Computation_of_cyclic_redundancy_checks
-    uint16_t crc = 0xffff;
-    uint16_t i = 0;
-    while (i < length)
-    {
-        crc ^= (data[i] << 8);
-        for (uint8_t j = 0; j < 8; j++)
-        {
-            if ((crc & 0x8000) > 0)
-                crc = (crc << 1) ^ crc16_polynomial;
-            else
-                crc = crc << 1;
-        }
-        i++;
+  // See https://en.wikipedia.org/wiki/Computation_of_cyclic_redundancy_checks
+  uint16_t crc = 0xffff;
+  uint16_t i = 0;
+  while (i < length) {
+    crc ^= (data[i] << 8);
+    for (uint8_t j = 0; j < 8; j++) {
+      if ((crc & 0x8000) > 0)
+        crc = (crc << 1) ^ crc16_polynomial;
+      else
+        crc = crc << 1;
     }
-    return crc;
+    i++;
+  }
+  return crc;
 }
 
 bool try_read_number_of_bytes_from_client(WiFiClient& client, uint8_t* buffer, uint16_t number_of_bytes) {
@@ -104,53 +105,94 @@ bool try_read_number_of_bytes_from_client(WiFiClient& client, uint8_t* buffer, u
     buffer[byte_ctr++] = (uint8_t)result;
   }
 
+  if (number_of_bytes != byte_ctr) {
+    DEBUG_PRINTF("Reading from client failed, got %d bytes\n", byte_ctr);
+    for (uint8_t i = 0; i < byte_ctr; i++)
+      DEBUG_PRINTF("0x%02x, ", buffer[i]);
+    DEBUG_PRINTLN("");
+  }
   return number_of_bytes == byte_ctr;
 }
 
-void handle_animation_frames_message(WiFiClient& client, uint16_t number_of_frames) {
-  uint8_t animation_frame_header_buffer[6];
-  uint16_t number_of_received_frames, last_received_frame, crc, calculated_crc, payload_length;
-  bool crc_match;
+void ensure_serial_input_buffer_empty() {
+  while (Serial.available() > 0)
+    Serial.read();
+}
 
+response_code get_response_from_target() {
+  uint8_t response_buffer[1];
+  uint8_t number_of_read_bytes = Serial.readBytes(response_buffer, 1);
+  if (number_of_read_bytes == 0)
+    return response_code::RESPONSE_TIMEOUT;
+  else if (number_of_read_bytes > 1 || response_buffer[0] > 0x06)
+    return response_code::RESPONSE_UNEXPECTED;
+  else
+    return (response_code)response_buffer[0];
+}
+
+response_code forward_transmission_header(uint8_t identifier, uint8_t* additional_data_buffer, uint8_t additional_data_length) {
+  ensure_serial_input_buffer_empty();
+  Serial.write(identifier);
+  if (additional_data_length > 0)
+    Serial.write(additional_data_buffer, additional_data_length);
+  return get_response_from_target();
+}
+
+response_code forward_message(uint8_t* header, uint8_t header_length, uint8_t* data, uint8_t data_length) {
+  ensure_serial_input_buffer_empty();
+  Serial.write(header, header_length);
+  Serial.write(data, data_length);
+  return get_response_from_target();
+}
+
+void read_and_forward_message(WiFiClient& client, uint8_t header_length, uint16_t number_of_frames) {
+  uint8_t message_header[header_length];
+  uint16_t number_of_received_frames, crc, calculated_crc, payload_length;
+  bool crc_match;
+  response_code response_from_target;
   number_of_received_frames = 0;
-  while (true) {
-    if (!try_read_number_of_bytes_from_client(client, animation_frame_header_buffer, 6)) {
-      DEBUG_PRINTLN("Reading of animation frame header failed");
+
+  while (number_of_received_frames < number_of_frames) {
+
+    while (!client.available())
+      delay(50);
+
+    if (!try_read_number_of_bytes_from_client(client, message_header, header_length)) {
+      DEBUG_PRINTLN("Reading of message header failed");
       return;
     }
 
-    last_received_frame = animation_frame_header_buffer[1] << 8 | animation_frame_header_buffer[0];
-    crc = animation_frame_header_buffer[3] << 8 | animation_frame_header_buffer[2];
-    payload_length = animation_frame_header_buffer[5] << 8 | animation_frame_header_buffer[4];
+    crc = message_header[1] << 8 | message_header[0];
+    payload_length = message_header[3] << 8 | message_header[2];
 
-    DEBUG_PRINT("Frame number: ");
-    DEBUG_PRINTLN(last_received_frame);
     DEBUG_PRINT("CRC: ");
     DEBUG_PRINTF("0x%02x\n", crc);
     DEBUG_PRINT("Payload length: ");
     DEBUG_PRINTLN(payload_length);
 
-    uint8_t frame_buffer[payload_length];
-    if (!try_read_number_of_bytes_from_client(client, frame_buffer, payload_length)) {
-      DEBUG_PRINTF("Reading of animation frame %d data failed", last_received_frame);
+    uint8_t data_buffer[payload_length];
+    if (!try_read_number_of_bytes_from_client(client, data_buffer, payload_length)) {
+      DEBUG_PRINTF("Reading of data buffer %d data failed\n", number_of_received_frames);
       return;
     }
 
-    calculated_crc = compute_crc16_checksum(frame_buffer, payload_length);
+    calculated_crc = compute_crc16_checksum(data_buffer, payload_length);
     crc_match = calculated_crc == crc;
     DEBUG_PRINT("Computed CRC: ");
     DEBUG_PRINTF("0x%02x (%s)\n", calculated_crc, crc_match ? "match" : "mismatch");
 
     if (!crc_match) {
       send_response(client, response_code::RESPONSE_CHECKSUM_MISMATCH);
-      delay(100);
+      delay(50);
       continue;
     }
 
-    number_of_received_frames++;
-
-    if (number_of_received_frames == number_of_frames)
+    response_from_target = forward_message(message_header, 4, data_buffer, payload_length);
+    send_response(client, response_from_target);
+    if (response_from_target != response_code::RESPONSE_OK)
       break;
+
+    number_of_received_frames++;
   }
 }
 
@@ -163,24 +205,42 @@ void handle_incoming_transmission(WiFiClient& client) {
 
   DEBUG_PRINTF("Message identifier: 0x%02X\n", identifier_buffer[0]);
 
+  uint16_t number_of_frames = 1;
+  uint8_t header_length;
+  response_code code;
+
   if (identifier_buffer[0] == animation_frames_message_identifier) {
-    uint8_t number_of_frames_buffer[2];
-    if (!try_read_number_of_bytes_from_client(client, number_of_frames_buffer, 2)) {
-      DEBUG_PRINTLN("Reading of number of frames field in header failed");
+    uint8_t additional_header_fields[6];
+    if (!try_read_number_of_bytes_from_client(client, additional_header_fields, 6)) {
+      DEBUG_PRINTLN("Reading of additional fields in header failed");
       return;
     }
 
-    send_response(client, response_code::RESPONSE_OK);
-    while (!client.available())
-      delay(50);
-
-    uint16_t number_of_frames = number_of_frames_buffer[1] << 8 | number_of_frames_buffer[0];
+    number_of_frames = additional_header_fields[1] << 8 | additional_header_fields[0];
     DEBUG_PRINTF("Number of frames: %d\n", number_of_frames);
-    handle_animation_frames_message(client, number_of_frames);
+    DEBUG_PRINTF("Number of bytes per frame: %d\n", additional_header_fields[3] << 8 | additional_header_fields[2]);
+    DEBUG_PRINTF("Number of rows: %d\n", additional_header_fields[4]);
+    DEBUG_PRINTF("Number of columns: %d\n", additional_header_fields[5]);
+
+    code = forward_transmission_header(animation_frames_message_identifier, additional_header_fields, 6);
+    header_length = 4;
   }
   else if (identifier_buffer[0] == game_of_life_config_message_identifier) {
-
+    code = forward_transmission_header(game_of_life_config_message_identifier, NULL, 0);
+    header_length = 4;
   }
+  else {
+    DEBUG_PRINTF("Got unknown message identifier (0x%02x)\n", identifier_buffer[0]);
+    return;
+  }
+
+  send_response(client, code);
+  if (code != response_code::RESPONSE_OK) {
+    DEBUG_PRINTF("Target did not accept transmission header (response_code: %d)\n", (uint8_t)code);
+    return;
+  }
+
+  read_and_forward_message(client, header_length, number_of_frames);
 }
 
 void loop() {
